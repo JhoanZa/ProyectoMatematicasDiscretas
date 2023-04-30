61ddf3fe0f4b996573254fa55239ab22b4df84e9
Jhoan Alejandro Zambrano Osorio <alejo.ax1306@gmail.com>
2023-04-30 01:50:15 -0500

    Agregar archivos de proyecto.

---
+++ Data/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoMatematicasDiscretas.Data
+{
+    public class Character
+    {
+        public char Symbol { get; set; }
+
+        [Range(0, 200, ErrorMessage = "El rango de pesos va de 0 a 200")]
+        public int Weight { get; set; }
+        
+        public string Code { get; set; } = string.Empty;
+
+        public Character? Left { get; set; } = null;
+        
+        public Character? Right { get; set; } = null;
+
+    }
+}
---
+++ Data/CharacterService.cs
@@ -0,0 +1,20 @@
+﻿namespace ProyectoMatematicasDiscretas.Data
+{
+    public class CharacterService
+    {
+        public Task<List<Character>> GetCharactersListAsync()
+        {
+            List<Character> characters = new();
+
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                characters.Add(new Character()
+                {
+                    Symbol = c,
+                    Weight = 0,
+                });
+            }
+            return Task.FromResult(characters);
+        }
+    }
+}
